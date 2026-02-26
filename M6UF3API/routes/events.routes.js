5ae05bc55e97fc696a46d89f1da1ec0db5c27fb8
bilal <bilal@smartaos.com>
2026-02-26 11:37:01 +0100

    feat:canvi ruta

--- M6UF3API/routes/events.routes.js
+++ M6UF3API/routes/events.routes.js
@@ -12,7 +12,7 @@ router.post("/", async (req, res) => {
 });
 
 // tots els events
-router.get("/", async (req, res) => {
+router.get("/ge", async (req, res) => {
   const events = await Event.find();
   res.json(events);
 });
