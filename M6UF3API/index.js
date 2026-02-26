81cbdb336f55cb0feee8e0b10f3fe1ecf222880d
bilal <bilal@smartaos.com>
2026-02-26 11:12:17 +0100

    feat:conectar en vercel

--- M6UF3API/index.js
+++ M6UF3API/index.js
@@ -6,15 +6,25 @@ import eventsRoutes from "./routes/events.routes.js";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
-//connexio atlas
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("Mongo connectat"))
-  .catch(err => console.log(err));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const clientOptions = {
+  family: 4
+};
+
+mongoose.connect(process.env.MONGO_URI, clientOptions)
+  .then(() => console.log("✅ Mongo connectat"))
+  .catch(err => console.log("❌ Error:", err.message));
 
 app.use("/api/events", eventsRoutes);
 
-app.listen(3000, () => {
-  console.log("Servidor escoltant a port 3000");
-});
+// Si quieres que funcione en local y en Vercel:
+if (process.env.NODE_ENV !== "production") {
+  app.listen(3000, () => {
+    console.log("🚀 Servidor escoltant a port 3000");
+  });
+}
+
+export default app;
--- M6UF3API/routes/events.routes.js
+++ M6UF3API/routes/events.routes.js
@@ -4,31 +4,27 @@ import Event from "../models/Event.js";
 const router = express.Router();
 
 
-// POST - afegir event
+// afegir event
 router.post("/", async (req, res) => {
   const event = new Event(req.body);
   await event.save();
   res.status(201).json(event);
 });
 
-
-//  GET - tots els events
+// tots els events
 router.get("/", async (req, res) => {
   const events = await Event.find();
   res.json(events);
 });
-
-
-//  GET - cercar per nom
+// cercar per nom
 router.get("/search/:text", async (req, res) => {
-  const events = await Event.find({
-    title: { $regex: req.params.text, $options: "i" }
+  const events = await Event.find ({
+    title: { $regex: req.params.text, $options:"i"}
   });
   res.json(events);
 });
 
-
-//  GET - entre dates
+//  entre dates
 router.get("/dates", async (req, res) => {
   const { inici, fi } = req.query;
 
@@ -45,12 +41,13 @@ router.get("/dates", async (req, res) => {
 
 //  DELETE
 router.delete("/:id", async (req, res) => {
-  await Event.findByIdAndDelete(req.params.id);
+   await Event.findByIdAndDelete(req.params.id);
   res.json({ msg: "Event eliminat" });
 });
 
 
-//  PUT - modificar
+
+// modificar
 router.put("/:id", async (req, res) => {
   const event = await Event.findByIdAndUpdate(
     req.params.id,
